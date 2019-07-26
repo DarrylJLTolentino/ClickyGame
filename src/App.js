b08dd1d995220684cff3445e8d3db760a5bf8f42
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-25 18:00:48 -0700

    Adding nav, hero, gamespace, and footer components within the app() in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
 import ClickyGame from "./pages/ClickyGame";
-import Nav from "./components/nav/index";
-import Hero from "./components/hero/index";
-import Footer from "./components/footer/index";
+import Nav from "./components/Nav/index";
+import Hero from "./components/Hero/index";
+import GameSpace from "./components/GameSpace/index";
+import Footer from "./components/Footer/index";
 import './App.css';
 
 function App() {
   return (
-    
+    <div>
+      <Nav />
+      <Hero />
+      <GameSpace />
+      <Footer />
+    </div>
   );
 }
 
