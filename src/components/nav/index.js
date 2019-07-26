92e736c3f6fcaf44c9e58215b0e7c3f9aaeae37b
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-26 00:31:58 -0700

    Commenting out image for now from nav component's index.js

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function Nav() {
+function Nav(props) {
     return (
         <div>
-            <ul className="nav">
+            <ul className="nav fixed-top">
                 <li className="nav-item">
                     <a className="nav-link active" href="/">
-                        <img src="https://banner2.kisspng.com/20180816/sps/kisspng-monster-hunter-world-logo-brand-font-portable-net-pierwsza-mini-sekcja-monster-hunter-world-crown-5b75c19f1870f9.6004538515344439351001.jpg" alt = "monster hunter world logo" />
+                        {/* <img src="https://banner2.kisspng.com/20180816/sps/kisspng-monster-hunter-world-logo-brand-font-portable-net-pierwsza-mini-sekcja-monster-hunter-world-crown-5b75c19f1870f9.6004538515344439351001.jpg" alt = "monster hunter world logo" /> */}
                     </a>
+                </li>
+                <li className = "nav-item">
+                    <p>Score: {props.score} / High Score: {props.high_score}</p>
                 </li>
             </ul>
         </div>
