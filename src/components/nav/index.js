32579325aea4259e26953836c6e6db4f666ea438
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-25 18:37:14 -0700

    Adding bootstrap nav within index.js of nav component

---
+++ src/components/nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <div>
+            <ul className="nav">
+                <li className="nav-item">
+                    <a className="nav-link active" href="/">
+                        <img src="https://banner2.kisspng.com/20180816/sps/kisspng-monster-hunter-world-logo-brand-font-portable-net-pierwsza-mini-sekcja-monster-hunter-world-crown-5b75c19f1870f9.6004538515344439351001.jpg" alt = "monster hunter world logo" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Nav;
