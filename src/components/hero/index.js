f06053c8befe539d37f3be13cb6bc82f13c844f7
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-26 00:34:47 -0700

    Adding jumbotron div with inline styling using props within index.js of hero component

---
+++ src/components/hero/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Hero(props) {
+    const heroImage = {
+        backgroundImage: `url("${props.src}")`,
+        backgroundSize: "auto 100%",
+        backgroundPosition: "right",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "black"
+    }
+
+    console.log(props.src);
+
+    return (
+        <div>
+            <div style = {heroImage} className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Monster Hunter!</h1>
+                    <p className="lead">Click on an image to slay the monster! Don't click the same monster or you lose!</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
